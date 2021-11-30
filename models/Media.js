71c34f572e10de3c78c0676234c91eb18c9b46ff
Eky Zulianto Arif <ekysambora@gmail.com>
2021-12-01 05:36:23 +0700

    add project

---
+++ models/Media.js
@@ -0,0 +1,28 @@
+module.exports = (sequelize, DataType) => {
+    const Media = sequelize.define('Media', {
+        id:{
+            type: DataType.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+            allowNull: false
+        },
+        image: {
+            type: DataType.STRING,
+            allowNull: false
+        },
+        createdAt:{
+            field:'created_at',
+            type: DataType.DATE,
+            allowNull: false
+        },
+        updatedAt:{
+            field:'updated_at',
+            type: DataType.DATE,
+            allowNull: false
+        }
+    },{
+        tableName: 'media'
+    });
+
+    return Media;
+}
